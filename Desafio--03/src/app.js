7e58c79e76a90cac0e4e8ece94c267f5dc0fcda9
Matias Osorio <matiasosorio@gmail.com>
2024-01-20 20:57:54 -0300

    Primer commit: Se creo app.js y productos.json. Se utilizo el ProductManager ya utilizado en los desafios anteriores. Funcionando todo

---
+++ Desafio--03/src/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const PUERTO = 8080;
+
+const ProductManager = require("./ProductManager.js");
+const productManager = new ProductManager("./src/products.json");
+
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+    try {
+        const limit = req.query.limit; // Obtén el límite de la consulta si se proporciona
+        const products = await productManager.getProducts();
+
+        if(limit) {
+            res.json(products.slice(0, limit));
+        } else {
+            res.json(products);
+        }
+    } catch (error) {
+        console.log("Error al obtener los productos", error);
+        res.status(500).json({ error: 'Error del servidor' });
+    }
+})
+
+//Traer un solo producto por ID
+
+app.get('/products/:pid', async (req, res) => {
+    let pid = req.params.pid;
+
+    try {
+        const product = await productManager.getProductById(parseInt(pid));
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ error: 'Producto no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'El producto solicitado no existe' });
+    }
+});
+
+app.listen(PUERTO, () => {
+    console.log(`Escuchando en el http://localhost${PUERTO}`);
+});

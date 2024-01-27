cee8cd5d12c60dcb6a4ad2679a3c7884a19b0de0
Matias Osorio <matiasosorio@gmail.com>
2024-01-27 17:57:46 -0300

    Primer commit: Primera pre entrega. Se dividieron los archivos en carpetas. Se creo cart-manager.js y las routes.

---
+++ Primera--pre--entrega/src/controllers/cart-manager.js
@@ -0,0 +1,77 @@
+const fs = require("fs").promises;
+
+class CartManager {
+    constructor(path) {
+        this.carts = [];
+        this.path = path;
+        this.lastId = 0;
+
+        //cargar los carritos almacenados
+        this.loadCarts();
+    }
+
+    async loadCarts() {
+        try {
+            const data = await fs.readFile(this.path, "utf-8");
+            this.carts = JSON.parse(data);
+            if(this.carts.length > 0) {
+                //verifica si hay un carrito creado
+                this.lastId = Math.max(...this.carts.map(cart => cart.id));
+                //metodo map para crear un nuevo array que solo tenga los identificadores del carrito y con Math.max se obtiene el mayor
+            }
+        } catch (error) {
+            console.error("Error al cargar los carritor", error);
+            // Si no existe el archivo lo crea
+            await this.loadCarts();
+        }
+    }
+
+    async saveCarts() {
+        await fs.writeFile(this.path, JSON.stringify(this.carts, null, 2));
+    }
+
+    async createCart() {
+        const newCart = {
+            id: ++this.lastId,
+            products: []
+        };
+
+        this.carts.push(newCart);
+
+        //Guardar el array
+        await this.saveCarts();
+        return newCart;
+    }
+
+    async getCartById(cartId) {
+        try {
+            const cart = this.carts.find(c => c.id === cartId);
+
+            if(!cart) {
+                throw new Error (`No existe un carrito con el id ${cartId} `);
+            }
+            return cart;
+
+        } catch (error) {
+            console.error("Error al obtener el carrito por ID", error);
+            throw error;
+        }
+    }
+
+    async addProductToCart(cartId, productId, quantity = 1) {
+        const cart = await this.getCartById(cartId);
+        const realProduct = cart.products.find(p => p.product === productId);
+
+        if(realProduct) {
+            realProduct.quantity += quantity;
+        } else {
+            cart,products.push({product: productId, quantity});
+        }
+
+        await this.saveCarts();
+        return cart;
+    }
+}
+
+module.exports = CartManager
+
---
+++ Primera--pre--entrega/src/routes/carts.router.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const CartManager = require("../controllers/cart-manager.js");
+const cartManager = new CartManager("./src/models/carts.json");
+
+//Crear nuevo carrito
+
+router.post("/", async (req, res) => {
+    try {
+        const newCart = await cartManager.createCart();
+        res.json(newCart);
+    } catch (error) {
+        console.error("Error al crear un nuevo carrito", error);
+        res.status(500).json({error: "Error del servidor"});
+    }
+});
+
+//Listar los productos que pertenecen a determinado carrito.
+
+router.get("/:cid", async (req, res) => {
+    const cartId = parseInt(req.params.cid);
+
+    try {
+        const cart = await cartManager.getCartById(cartId);
+        res.json(cart.products);
+    } catch (error) {
+        console.error("Error al obtener el carrito", error);
+        res.status(500).json({error: "Errro del servidor"});
+    }
+});

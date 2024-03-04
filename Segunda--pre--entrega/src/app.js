1d333cfd3d37b1b623ce7cf02a8ee5a53938bd2f
Matias Osorio <matiasosorio@gmail.com>
2024-03-03 21:42:29 -0300

    Primer commit: Se crearon los metodos eliminar, actualizar y vaciar productos del cart

---
+++ Segunda--pre--entrega/src/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const PUERTO = 8080;
+
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
+const handlebars = require("express-handlebars");
+const socket = require("socket.io");
+require("./database.js");
+
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./src/public"));
+
+//Handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+//Rutas
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
+
+app.listen(PUERTO, () => {
+    console.log(`Escuchando en el http://localhost:${PUERTO}`);
+});
+
+
+
+
+
+
